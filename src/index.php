b25ddeeddf8a4e458133f31e3ccc3a99e09cbec7
cris <cristian@enve.io>
2022-05-31 10:44:04 -0300

    second commit

--- src/index.php
+++
@@ -1,3 +0,0 @@
-<?php echo '<p>Hello World!!</p>'; ?> 
- <?php echo '<p>Welcome to PHPApp running inside Docker container!</p>'; ?> 
- <?php echo '<p>We have automated Docker image creation and deployment!!!</p>'; ?> 
